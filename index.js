50537c780cbbe0a4d3bbfcb9b35c2fd745f155ba
sahadat <shmanik14@yahoo.com>
2021-04-18 13:01:36 +0600

    add api

--- index.js
+++ index.js
@@ -17,13 +17,91 @@ const port = process.env.PORT || 4000
 client.connect(err => {
     const serviceCollection = client.db("weFix").collection("services");
     const testimonialCollection = client.db("weFix").collection("testimonials");
+    const orderCollection = client.db("weFix").collection("orders");
 
     app.post('/addService', (req,res) => {
         const service = req.body;
-        paintCollection.insertOne(service)
+        serviceCollection.insertOne(service)
         .then(result => {
             console.log(result);
             res.send(result.insertedCount > 0)
+        })
+    })
+
+    app.post('/addOrder', (req,res) => {
+        const order = req.body;
+        orderCollection.insertOne(order)
+        .then(result => {
+            console.log(result);
+            res.send(result.insertedCount > 0)
+        })
+      })
+
+    app.post('/addTestimonial', (req,res) => {
+        const testimonial = req.body;
+        testimonialCollection .insertOne(testimonial)
+        .then(result => {
+            console.log(result);
+            res.send(result.insertedCount > 0)
+        })
+    })
+
+    app.get('/book/:id', (req, res) => {
+        serviceCollection.find({_id: objectId(req.params.id)})
+        .toArray((err, documents) => {
+          res.send(documents[0]);
+        })
+      })
+
+      app.get('/service/:id', (req, res) => {
+        serviceCollection.find({_id: objectId(req.params.id)})
+        .toArray((err, documents) => {
+          res.send(documents[0]);
+        })
+      })
+
+    app.get('/services', (req, res) => {
+        serviceCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+    })
+    app.get('/testimonials', (req, res) => {
+        testimonialCollection.find({})
+        .toArray((err, documents) => {
+            res.send(documents);
+        })
+    })
+
+    app.get("/orders", (req,res) => {
+        orderCollection.find({})
+        .toArray((err, documents) => {
+        res.send(documents);
+        })
+    })
+
+    app.get("/userOrders", (req,res) => {
+        orderCollection.find({email: req.query.email})
+        .toArray((err, documents) => {
+        res.send(documents);
+        })
+    })
+
+    app.delete("/delete/:id", (req, res) => {
+        serviceCollection.findOneAndDelete({_id: objectId(req.params.id)})
+        .then(result => {
+          console.log(result)
+          res.send(result.deletedCount > 0);
+        })
+      })
+    
+    app.patch("/update/:id", (req,res) => {
+        collection.updateOne({_id: objectId(req.params.id)},{
+            $set: {pName: req.body.name, description: req.body.description}
+            
+        })
+        .then(result => {
+            res.send(result.modifiedCount > 0);
         })
     })
 
@@ -31,7 +109,7 @@ client.connect(err => {
 })
 
 app.get('/', (req, res) => {
-    res.send('Hello Painting Precision')
+    res.send('Hello WeFix')
   })
   
 app.listen(port)
